2b61e5865b9cd411ca8582c4ccf7b497c3e837f8
uhoney <t2maju06@students.oamk.fi>
2024-09-19 15:11:13 +0300

    viikkotehtava 3 valmis

--- app/src/main/java/com/example/loginform/MainActivity.kt
+++ app/src/main/java/com/example/loginform/MainActivity.kt
@@ -4,12 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.loginform.ui.theme.LoginformTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,8 +40,9 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting()
+                )
+                {
+                    Login()
                 }
             }
         }
@@ -30,8 +50,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting() {
-    Text(
-        text = "Hello world!",
-    )
+fun Login() {
+
+    var usernameInput = remember { mutableStateOf("") }
+    var passwordInput = remember { mutableStateOf("") }
+
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier
+            .systemBarsPadding()
+            .padding(24.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.title),
+            modifier = Modifier.padding(bottom = 16.dp),
+            color = MaterialTheme.colorScheme.primary,
+            fontSize = 24.sp,
+        )
+        OutlinedTextField(
+            value = usernameInput.value,
+            onValueChange = { usernameInput.value = it },
+            label = { Text(stringResource(R.string.user_name)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            trailingIcon = {
+                Icon(Icons.Default.Email, contentDescription = "Email Icon")
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = passwordInput.value,
+            onValueChange = { passwordInput.value = it },
+            label = { Text(stringResource(R.string.password)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            visualTransformation = PasswordVisualTransformation(),
+            trailingIcon = {
+                Icon(Icons.Default.Lock, contentDescription = "Lock Icon")
+            },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.button_label))
+        }
+    }
 }
